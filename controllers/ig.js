883ff04438fbffbec270057493e8b97bc64253dd
Dellas <87001693+itsmedell@users.noreply.github.com>
2021-08-21 00:22:00 +0800

    Create ig.js

--- controllers/ig.js
+++ controllers/ig.js
@@ -9,67 +9,75 @@ const options = {
   session: "sessionid=" + sID
 };
 
-async function igStalk(username = 'instagram') {
-  return new Promise((resolve, reject) => {
-    try {
+async function igStalk(req, res) {
+	const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    const username = req.query.username;
+    if (username === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter username`
+    });
       ig.getUserMeta(username, options)
-        .then((data) => {
-          resolve({
-            result: {
-              profile: data.graphql.user.profile_pic_url,
-              profilehd: data.graphql.user.profile_pic_url_hd,
-              fullname: data.graphql.user.full_name,
-              private: data.graphql.user.is_private,
-              verified: data.graphql.user.is_verified,
-              bio: data.graphql.user.biography,
-              follower: data.graphql.user.edge_followed_by.count,
-              following: data.graphql.user.edge_follow.count,
-              conneted_fb: data.graphql.user.connected_fb_page,
-              videotimeline: data.graphql.user.edge_felix_video_timeline.count,
-              timeline: data.graphql.user.edge_owner_to_timeline_media.count,
-              savedmedia: data.graphql.user.edge_saved_media.count,
-              collections: data.graphql.user.edge_media_collections.count
-            }
+        .then(result => {
+        res.status(200).send({
+        	  status: 200,
+              profile: result.graphql.user.profile_pic_url,
+              profilehd: result.graphql.user.profile_pic_url_hd,
+              fullname: result.graphql.user.full_name,
+              private: result.graphql.user.is_private,
+              verified: result.graphql.user.is_verified,
+              bio: result.graphql.user.biography,
+              follower: result.graphql.user.edge_followed_by.count,
+              following: result.graphql.user.edge_follow.count,
+              conneted_fb: result.graphql.user.connected_fb_page,
+              videotimeline: result.graphql.user.edge_felix_video_timeline.count,
+              timeline: result.graphql.user.edge_owner_to_timeline_media.count,
+              savedmedia: result.graphql.user.edge_saved_media.count,
+              collections: result.graphql.user.edge_media_collections.count
           });
-        })
-        .catch((err) =>
-          reject({
-            message: "akun tidak di temukan atau username tidak valid"
-          })
-        );
-    } catch (err) {
-      reject(err);
-    }
-  });
+        }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Username tidak ditemukan!'});
+    });
 }
 
-async function igDownload(url) {
-  return new Promise((resolve, reject) => {
-    if (url === 'undefined') { 
-      reject({
-        msg: 'Masukkan URL nya.'
-      })
-    }
-    url = url.split('?')[0];
-      try {
+async function igDownload(req, res) {
+  const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    const url = req.query.url;
+    if (url === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter url`
+    });
       ig.getPostMeta(url, options)
-      .then(data =>
-        resolve({
-          id: data.graphql.shortcode_media.id,
-          shortCode: data.graphql.shortcode_media.shortcode,
-          caption: data.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text,
-          url: data.graphql.shortcode_media.display_url
+      .then(result => {
+        res.status(200).send({
+          status: 200,
+          id: result.graphql.shortcode_media.id,
+          shortCode: result.graphql.shortcode_media.shortcode,
+          caption: result.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text,
+          url: result.graphql.shortcode_media.display_url
         })
-      )
-    } 
-    catch(err) {
-      reject({
-        status: 406,
-        creator: '@only_fxc7',
-        msg: "Link Tidak Valid"
-      })
-    }
-  })
+      }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Username tidak ditemukan!'});
+    });
 }
 
 module.exports = {
