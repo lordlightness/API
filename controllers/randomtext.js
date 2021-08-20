4979950c2ea7900ad82643b8e96c0375557b06af
Dellas <87001693+itsmedell@users.noreply.github.com>
2021-08-20 22:10:34 +0800

    Create randomtext.js

--- controllers/randomtext.js
+++ controllers/randomtext.js
@@ -146,5 +146,22 @@ async function motivasi(req, res) {
         res.status(500).send({status: 500, message: 'Internal Server Error'});
     });
 }
-
-module.exports = {cakLontong, tebakbucin, quotes, bijak, fakta, ptl, motivasi };
+async function cyberspace(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey)
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileTxt('./lib/data/cyberspace.txt').then(result => {
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+module.exports = {cakLontong, tebakbucin, quotes, bijak, fakta, ptl, motivasi, cyberspace };
