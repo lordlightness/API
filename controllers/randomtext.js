4ed2bcacba2118df506c5d7dfa42d37cbb945b13
Dellas <87001693+itsmedell@users.noreply.github.com>
2021-08-20 20:15:55 +0800

    Create randomtext.js

--- controllers/randomtext.js
+++ controllers/randomtext.js
@@ -25,6 +25,29 @@ async function cakLontong(req, res) {
     });
 }
 
+async function tebakbucin(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileJson('./lib/data/tebakbucin.json').then(result => {
+        res.status(200).send({
+            status: 200, 
+            Pertanyaan: result.Pertanyaan, 
+            jawaban: result.jawaban
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
 async function quotes(req, res) {
     const apikey = req.query.apikey;
     if (apikey === undefined) return res.status(404).send({
