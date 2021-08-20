bc90f500975bbb858c3d6e1ce5f0a8d9006847cc
Dellas <87001693+itsmedell@users.noreply.github.com>
2021-08-21 00:59:50 +0800

    Create api.js

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { cekKey } = require('../database/db'); 
 const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
 const { cakLontong, tebakbucin, bijak, quotes, fakta, ptl, motivasi, cyberspace } = require('../controllers/randomtext');
-const { igStalk, igDownload } = require('../controllers/ig');
+const { igstalk, igdownload } = require('../controllers/ig');
 router.get('/checkkey', async (req, res) => {
     const apikey = req.query.apikey;
     if (apikey === undefined) return res.status(404).send({
@@ -40,8 +40,8 @@ router.get('/motivasi', motivasi);
 
 router.get('/cyberspace', cyberspace);
 
-router.get('/igstalk', igStalk);
+router.get('/igstalk', igstalk);
 
-router.get('/igDownload', igDownload);
+router.get('/igdl', igdownload);
 
 module.exports = router;
